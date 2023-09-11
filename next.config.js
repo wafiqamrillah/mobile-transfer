63bd3a7a3d51e74efb46ef743399b88caaa85f12
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-09-11 11:38:51 +0700

    feat(init): :sparkles: Install packages and config apps
    Install packages :
    - Font Awesome 6
    - Axios
    - SweetAlert 2
    - SWR
    - TailwindCSS 3.2.4
    
    Configuration :
    - globals.css
    - next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   reactStrictMode: true,
 }
 
